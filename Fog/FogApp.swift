4d9ec0c95b45a6f041375c92c4e700117992266e
Akshat2923 <akshat.saladi@gmail.com>
2026-02-21 17:24:09 -0500

    Initial Commit

---
+++ Fog/FogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FogApp.swift
+//  Fog
+//
+//  Created by Akshat  Saladi on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct FogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
